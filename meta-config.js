b47e621bd73232cf8b7c733dbb417f6325e672c0
Haon <88240193+msung99@users.noreply.github.com>
2024-07-30 09:30:19 +0900

    url 수정

--- meta-config.js
+++ meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: `harmony.blog`,
     description: `하모니 팀의 기술 블로그 입니다. 🌈`,
     author: `harmony`,
-    siteUrl: `https://main--kakaotech-moheng.netlify.app/`,
+    siteUrl: `https://kakaotech-harmony.netlify.app/`,
     keywords: [`harmony`, 'tech-blog'], 
     repo: 'kakaotech-25/harmony-tech-blog',
     gtag: '', // with. Google Analytics
